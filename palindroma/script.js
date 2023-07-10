68f4ef88fab679acecb99ca0dbdc49f737689380
ElyPoli <itsos.poli@gmail.com>
2023-07-10 14:30:58 +0200

    created files

---
+++ palindroma/script.js
@@ -0,0 +1,5 @@
+/*
+Palidroma:
+- Chiedere all’utente di inserire una parola
+- Creare una funzione per capire se la parola inserita è palindroma (una parola che si legge allo stesso modo anche da destra versi sinistra, ad esempio anna, otto)
+*/
