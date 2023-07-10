b0ef728cd5f9ab67cfb887e5c6d75e6b995b5b35
ElyPoli <itsos.poli@gmail.com>
2023-07-10 15:06:58 +0200

    added js code - ex. Pari e dispari

--- pari-dispari/script.js
+++ pari-dispari/script.js
@@ -6,3 +6,55 @@ Pari e Dispari:
 - Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 - Dichiariamo chi ha vinto.
 */
+
+// Dichiaro variabili
+const choice = prompt("Scegli: pari o dispari?");
+const numberUser = parseInt(prompt("Inserisci un numero da 1 a 5:"));
+let numberPc = randomGenerator (5, 1); // Invoco la funzione che genera un numero casuale
+let sum, result, winner;
+
+console.log("Scelta utente: " + choice);
+console.log("Numero utente: " + numberUser);
+console.log("Numero pc: " + numberPc);
+
+// Controllo input utente
+if (isNaN(numberUser) || numberUser < 1 || numberUser > 5) {
+    alert("Si è verificato un errore");
+    location.reload();
+}
+
+// Somma dei due numeri
+sum = numberUser + numberPc;
+
+console.log("Somma dei due numeri " + sum);
+
+result = evenOdd(sum); // Invoco la funzione che stabilisce se un numero è pari o dispari
+
+console.log("Il numero è: " + result);
+
+// Stabilisco chi ha vinto
+if (result === choice) {
+    winner = "utente";
+} else {
+    winner = "computer";
+}
+
+console.log("Il vincitore è: " + winner);
+
+// Funzione che genera numeri random da 1 a 5
+function randomGenerator (max, min) {
+    randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    return randomNumber;
+}
+
+// Funzione che stabilisce se un numero è pari o dispari
+function evenOdd (sumNumber) {
+    let numberEvenOdd = "dispari";
+
+    if (sumNumber % 2 === 0) {
+        numberEvenOdd = "pari";
+    }
+
+    return numberEvenOdd;
+}
